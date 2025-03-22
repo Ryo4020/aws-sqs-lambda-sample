828bfe513900231ea607ab1b398723ba95e2d6ff
Ryo4020 <gryo.1440@gmail.com>
2025-03-22 23:04:40 +0900

    feat: set up permissions boundary

--- bin/aws-cdk-policy-attachment.ts
+++ bin/aws-cdk-policy-attachment.ts
@@ -2,10 +2,12 @@
 import * as cdk from 'aws-cdk-lib';
 import { QueueStack } from '../lib/queue-stack';
 
+const servicePrefix = 'PolicyAttachmentSample';
+
 const env = {
   account: process.env.CDK_ACCOUNT,
   region: process.env.CDK_REGION
 }
 
 const app = new cdk.App();
-new QueueStack(app, 'QueueStack', { env });
+new QueueStack(app, servicePrefix + 'QueueStack', { env });
